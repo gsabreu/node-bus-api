180ac70c4b1373e7ef55257c83d706e4ccb64a09
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-04-04 16:50:56 -0300

    Improving test for password

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         stausCode: 400
       }
@@ -14,6 +14,17 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.stausCode).toBe(400)
+  })
+
+  test('Should return 400 if password isnt provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email'
       }
     }
     const httpResponse = sut.route(httpRequest)
