e37a3dc93db6c7986bdf5f4cbc14c4dc0c441bae
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-04-05 21:23:03 -0300

    Improving email validator with trows

--- src/utils/helpers/email-validator.spec.js
+++ src/utils/helpers/email-validator.spec.js
@@ -1,5 +1,6 @@
 const EmailValidator = require('./email-validator')
 const validator = require('validator')
+const MissingParamError = require('../errors/missing-param-error')
 
 const createSut = () => {
   return new EmailValidator()
@@ -22,5 +23,10 @@ describe('Email Validator', () => {
     const sut = createSut()
     sut.isValid('any_email@mail.com')
     expect(validator.email).toBe('any_email@mail.com')
+  })
+
+  test('Should thorw if email arent provide', async () => {
+    const sut = createSut()
+    expect(() => { sut.isValid() }).toThrow(new MissingParamError('email'))
   })
 })
