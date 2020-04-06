4480602c9636da6c0ffb349c48a15f083eb76ba3
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-04-05 21:39:33 -0300

    Using factory to create mocks

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,16 +1,20 @@
 const jwt = require('jsonwebtoken')
 const TokenGenerator = require('./token-generator')
 
+const createSut = () => {
+  return new TokenGenerator()
+}
+
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
-    const sut = new TokenGenerator()
+    const sut = createSut()
     jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
   })
 
   test('Should return token if JWT returns token', async () => {
-    const sut = new TokenGenerator()
+    const sut = createSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
   })
