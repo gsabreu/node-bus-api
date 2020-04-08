255d86bcf18e2654fcf4eeead5aff9c77a2c32f7
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-04-07 22:57:53 -0300

    Testing html content-type

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -4,11 +4,22 @@ const app = require('../config/app')
 describe('Content-Type Middleware', () => {
   test('Should return json content-type as default', async () => {
     app.get('/test_content_type', (req, resp) => {
-      resp.send({})
+      resp.send('')
     })
 
     await request(app)
       .get('/test_content_type')
       .expect('content-type', /json/)
+  })
+
+  test('Should return html content-type', async () => {
+    app.get('/test_content_type_html', (req, resp) => {
+      resp.type('html')
+      resp.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_html')
+      .expect('content-type', /html/)
   })
 })
