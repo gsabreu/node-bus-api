5222634dcc5059635ce4f0583780403ddde7ae85
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-04-10 13:09:44 -0300

    Creating router composer

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xxx/bus-node-api'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xxx/bus-node-api',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
